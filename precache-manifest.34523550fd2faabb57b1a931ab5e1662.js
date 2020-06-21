696ead96437b33df12585f66abb466d8214ead9d
sejlap <sejlapljakic233@gmail.com>
2020-06-21 20:26:30 +0200

    Updates

---
+++ precache-manifest.34523550fd2faabb57b1a931ab5e1662.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5480dcbc79e8506a57e3ef9db2b20b23",
+    "url": "/website/index.html"
+  },
+  {
+    "revision": "8cfe1016b8c8ec0f5332",
+    "url": "/website/static/css/main.60edc659.chunk.css"
+  },
+  {
+    "revision": "5d9bc3aa73ceef457048",
+    "url": "/website/static/js/2.3c081401.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/website/static/js/2.3c081401.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8cfe1016b8c8ec0f5332",
+    "url": "/website/static/js/main.06d258ca.chunk.js"
+  },
+  {
+    "revision": "b56fd8800aeaade8aa1a",
+    "url": "/website/static/js/runtime-main.1c8d7277.js"
+  },
+  {
+    "revision": "ba73a071dd00e22dcddeb4d2655d3983",
+    "url": "/website/static/media/background.ba73a071.jpg"
+  },
+  {
+    "revision": "a9d84ab6a15828b8c46cdc604872cc25",
+    "url": "/website/static/media/slika.a9d84ab6.jpg"
+  }
+]);
